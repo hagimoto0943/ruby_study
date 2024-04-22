1599ae77245def8dbe6ad8b7a8533491c9e3f676
hagimoto0943 <hagi.san.09275571@gmail.com>
2024-04-22 23:50:39 +0900

    fixed

---
+++ object.rb
@@ -0,0 +1,24 @@
+class Foo
+  def initialize(a)
+    @a = a
+  end
+  def method1
+    @a
+  end
+end
+
+foo1 = Foo.new(1)
+foo2 = Foo.new(2)
+
+p foo1.method1 # => 1
+p foo2.method1 # => 2
+
+class FooExt < Foo
+  def initialize(a)
+    @b = b
+    super(a)
+  end
+  def mothod2(c)
+    @a + @b + c
+  end
+end
